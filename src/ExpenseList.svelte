62ee15f16b2e1d01848f3ad6fb7bef2df504590c
Ran You <ran.you8311@gmail.com>
2021-03-01 21:12:19 -0800

    complete

--- src/ExpenseList.svelte
+++ src/ExpenseList.svelte
@@ -2,6 +2,7 @@
     import Title from './Title.svelte';
     import Expense from './Expense.svelte';
     import { blur,fly,fade } from 'svelte/transition';
+    import { flip } from 'svelte/animate';
     export let expenses=[];
 </script>
 <style>
@@ -13,9 +14,10 @@
 <section>
     <Title title='expense list'/>
     <ul>
-        {#each expenses as expense,index (expense.id)}
-        <div transition:fly="{{x:200, delay:(index)*700 
-        }}">
+        {#each expenses as expense,index (expense.id)
+        }
+        <div transition:fly={{x:200, delay:(index)*700 
+        }}  animate:flip>
         <Expense {...expense} />
         </div>
         {:else}
